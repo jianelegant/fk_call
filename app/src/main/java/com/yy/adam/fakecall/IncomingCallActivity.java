65b27e19999902cff68db0f57352d4586302b3cc
jianelegant <jian.elegant@gmail.com>
2018-08-15 20:28:39 +0800

    ani

--- app/src/main/java/com/yy/adam/fakecall/IncomingCallActivity.java
+++ app/src/main/java/com/yy/adam/fakecall/IncomingCallActivity.java
@@ -1,11 +1,15 @@
 package com.yy.adam.fakecall;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class IncomingCallActivity extends AppCompatActivity {
+
+    ImageView mLeftArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +17,15 @@ public class IncomingCallActivity extends AppCompatActivity {
         Util.dismissKeyguard(this);
         setContentView(R.layout.activity_incoming_call);
         Util.hideSystemNaviUI(this);
+
+        initViews();
+    }
+
+    private void initViews() {
+        mLeftArrow = findViewById(R.id.id_left_arrow);
+        ObjectAnimator animation = ObjectAnimator.ofFloat(mLeftArrow, "translationX", 100f);
+        animation.setDuration(1000);
+        animation.start();
     }
 
     public static void callStart(Activity activity) {
