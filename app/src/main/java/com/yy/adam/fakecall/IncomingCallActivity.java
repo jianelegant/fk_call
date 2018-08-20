19d90f8dc7b392bcb04854b435054c22ed87fac7
jianelegant <jian.elegant@gmail.com>
2018-08-20 15:47:28 +0800

    anim

--- app/src/main/java/com/yy/adam/fakecall/IncomingCallActivity.java
+++ app/src/main/java/com/yy/adam/fakecall/IncomingCallActivity.java
@@ -4,12 +4,20 @@ import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
+import java.lang.ref.WeakReference;
+
 public class IncomingCallActivity extends AppCompatActivity {
 
+    private static final int HANDLER_MSG_ANIM = 1;
+    private static final int HANDLER_ANIM_DUR = 1000;
+    private Handler mHandler;
+
     ImageView mLeftArrow;
+    ObjectAnimator mLeftAnima;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,20 +25,60 @@ public class IncomingCallActivity extends AppCompatActivity {
         Util.dismissKeyguard(this);
         setContentView(R.layout.activity_incoming_call);
         Util.hideSystemNaviUI(this);
-
+        initHandler();
         initViews();
+    }
+
+    private void initHandler() {
+        mHandler = new Handler(this);
     }
 
     private void initViews() {
         mLeftArrow = findViewById(R.id.id_left_arrow);
-        ObjectAnimator animation = ObjectAnimator.ofFloat(mLeftArrow, "translationX", 100f);
-        animation.setDuration(1000);
-        animation.start();
+        startAnim();
+    }
+
+    private void startAnim() {
+        if(mHandler.hasMessages(HANDLER_MSG_ANIM)) {
+            mHandler.removeMessages(HANDLER_MSG_ANIM);
+        }
+        loopAnim();
+    }
+
+    private void loopAnim() {
+        if(null == mLeftAnima) {
+            mLeftAnima = ObjectAnimator.ofFloat(mLeftArrow, "translationX", 0, 100f, 0);
+            mLeftAnima.setDuration(HANDLER_ANIM_DUR);
+        }
+        mLeftAnima.start();
+        mHandler.sendEmptyMessageDelayed(HANDLER_MSG_ANIM, HANDLER_ANIM_DUR);
     }
 
     public static void callStart(Activity activity) {
         if(null != activity) {
             activity.startActivity(new Intent(activity, IncomingCallActivity.class));
+        }
+    }
+
+    static class Handler extends android.os.Handler {
+        WeakReference<IncomingCallActivity> activityWeakReference;
+
+        public Handler(IncomingCallActivity activity) {
+            super();
+            this.activityWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case HANDLER_MSG_ANIM:
+                    if(null != activityWeakReference.get()) {
+                        activityWeakReference.get().loopAnim();
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
